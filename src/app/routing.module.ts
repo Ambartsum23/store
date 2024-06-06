7c79d509dc80a92992284bc21aeb7b1e97f4442d
Ambartsum Karapetyan <ambartsumkarapetyan@gmail.com>
2024-06-06 15:11:14 +0400

    first commit

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -10,15 +10,12 @@ import { AuthComponent } from './auth/auth.component';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'home', children: [
-      { path: 'user', component: HomeComponent } 
-    ] },
   { path: 'all', component: AllproductsComponent },
   { path: 'catalog', component: CatalogComponent },
   { path: 'info/:id', component: InfoComponent },
   { path: 'cart', component: CartComponent },
   { path: 'auth/:action', component: AuthComponent },
-  { path: '**', component: CatalogComponent },
+  { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
